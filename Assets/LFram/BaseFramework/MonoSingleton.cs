344ebfdc06ce5048f461952cec61d125a87d572e
Unknown <fengliu17@qq.com>
2019-01-02 18:00:36 +0800

    整理代码 +1

---
+++ Assets/LFram/BaseFramework/MonoSingleton.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary> MonoBehaviour单例基类 </summary>
+public class MonoSingleton<T> : MonoBehaviourEx where T : MonoBehaviour   ///？？？？
+{
+    static T mInstance;
+
+    public static T Instance {
+        get {
+            if (null == mInstance) {
+                mInstance = FindObjectOfType(typeof(T)) as T;  //查询T类型的对象
+                if (null == mInstance) {
+                    //if (Application.isPlaying) {
+                    //    GameObject go = new GameObject("_" + typeof(T).Name);
+                    //    mInstance = go.AddComponent<T>();
+                    //}
+                    if (null == mInstance) {
+                        if (Application.isPlaying && !Application.isEditor) {
+                            Debug.LogError(typeof(T) + " 没有实例化！"); 
+                        }
+                    }
+                }
+            } else if (mInstance.gameObject == null) {
+                Debug.LogError(typeof(T) + " 没有清理干净！"); 
+            }
+
+            return mInstance;
+        }
+    }
+
+    void Awake() {
+#if Debug_Init_TimeWatch
+        HelpFun.timeWatch.Reset();
+        HelpFun.timeWatch.Start();
+        OnAwake();
+        HelpFun.timeWatch.Stop();
+        if (HelpFun.timeWatch.Elapsed.TotalSeconds > 0.05f) {
+            DebugManager.Log(DebugerName.Public, string.Format("MonoBehaviour<{0}>OnAwake：{1}", this.ToString(), HelpFun.timeWatch.Elapsed));  
+        }
+#else
+        OnAwake();
+#endif
+        //RegisterNotificationListener(true);
+        DontDestroyOnLoad(this);
+    }
+
+    void Start() {
+#if Debug_Init_TimeWatch
+        HelpFun.timeWatch.Reset();
+        HelpFun.timeWatch.Start();
+        OnStart();
+        HelpFun.timeWatch.Stop();
+        if (HelpFun.timeWatch.Elapsed.TotalSeconds > 0.05f) {
+            DebugManager.Log(DebugerName.Public, string.Format("MonoBehaviour<{0}>OnStart：{1}", this.ToString(), HelpFun.timeWatch.Elapsed)); 
+        }
+#else
+        OnStart();
+#endif
+    }
+
+    void OnDestroy() {
+        RegisterNotificationListener(false);
+    }
+
+
+    protected virtual void OnAwake() {
+    }
+
+    protected virtual void OnStart() {
+    }
+
+    protected virtual void OnOnDestroy() {
+    }
+
+    protected virtual void RegisterNotificationListener(bool isAdd = true) {
+
+    }
+}
+
+public abstract class MonoBehaviourEx : MonoBehaviour
+{
+
+
+}
---
+++ Assets/LFram/BaseFramework/Singleton.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary> 普通单例基类(没有MonoBehaviour) </summary>
+public class Singleton<T> where T : new()
+{
+    protected static T mInstance;  //通过静态变量获取类实例
+
+    public static T Instance {
+        get {
+            if (mInstance == null) {
+#if Debug_Init_TimeWatch
+                HelpFun.timeWatch.Reset();
+                HelpFun.timeWatch.Start();
+                mInstance = new T();
+                HelpFun.timeWatch.Stop();
+                if (HelpFun.timeWatch.Elapsed.TotalSeconds > 0.1f) {
+                    Debug.Log(string.Format("Singleton<{0}>Ctor：{1}", mInstance.ToString(), HelpFun.timeWatch.Elapsed));  
+                }
+#else
+                mInstance = new T();
+#endif
+            }
+            return mInstance;
+        }
+    }
+
+    public static void New() {
+        mInstance = new T();
+    }
+}
